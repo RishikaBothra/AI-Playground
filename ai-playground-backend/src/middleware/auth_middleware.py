ea0201ee28bec6f0f2bf2ed3110245dfd4c521cc
Rishika Bothra <rshkbothra@gmail.com>
2026-01-02 14:24:50 +0530

    refactor: clean up auth_middleware

--- ai-playground-backend/src/middleware/auth_middleware.py
+++ ai-playground-backend/src/middleware/auth_middleware.py
@@ -1,32 +1,3 @@
-# from fastapi import Request, HTTPException
-# from fastapi.responses import JSONResponse
-# from auth.jwthandler import verify_access_token
-
-# async def auth_middleware(request:Request,call_next):
-
-#     public_paths = ["/signin", "/signup", "/"]
-#     if request.url.path in public_paths:
-#         response = await call_next(request)
-#         return response
-    
-#     # auth_header = request.headers.get("Authorization")
-#     # if not auth_header:
-#     #     raise HTTPException(status_code=401, detail="Authorization header missing")
-#     # try:
-#     token = request.headers.get("Authorization")
-#     if not token:
-#         raise HTTPException(status_code=401, detail="Authorization header missing")
-
-#     # except:
-#     #     raise HTTPException(status_code=401, detail="Invalid authorization header format")
-    
-#     # payload = verify_access_token(token)
-#     # if not payload:
-#     #     raise HTTPException(status_code=401, detail="Invalid or expired token")
-    
-#     # request.state.user = payload["user_id"]
-#     response = await call_next(request)
-
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from src.service.jwthandler import verify_access_token
